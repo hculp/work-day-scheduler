55868906fbaf8d6176604d117390dd645182a5be
Houston Culpepper <howacul@gmail.com>
2023-04-20 22:42:43 -0400

    added comments, updated README, and fixed time format

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# work-day-scheduler
+# 05 Third-Party APIs: Work Day Scheduler
+
+## Description
+
+This is a simple work day scheduler. It updates and displays the current day at the top of the calendar. Each timeblock is given a unique color based on the current time, such that
+past time is grey, present time is orange, and future is green. After a user inputs information in the text field for a timeblock and clicks the save button, that event is stored, so
+the user can refresh the page and keep it in their planner. If a user does not click save for a timeblock for input event information, then that event is not saved and disappears
+upon refresh.
+
+## Link and preview
+
+https://github.com/hculp/work-day-scheduler
+
+[!Alt txt](./assets/images/workdayscheduler_demo.gif "work day scheduler demo")
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
+// All the script code is wrapped in a jQuery function to ensure that all the DOM elements load before performing the script.
 $(function () {
   var saveBtn = $('.saveBtn');
   var currentDay = $('#currentDay');
   var timeBlock = $('.time-block')
 
+  // An event click for when the save button is pressed and the funnction saveInput is called.
   saveBtn.click(saveInput);
 
+  // This function updates the class for a time slot compared to the current hour with either future, present, or pass.
   function applyTimeClass() {
     var hour = dayjs().hour();
 
@@ -25,6 +23,7 @@ $(function () {
     })
   }
  
+  // After the event clicker calls the saveInput function, then the time slot and text content is stored in local storage.
   function saveInput(event) {
     event.preventDefault();
     var time = $(this).siblings().text();
@@ -32,15 +31,16 @@ $(function () {
     localStorage.setItem(time,content);
   }
 
-
-    $(".hour").each(function() {
-        var currentTimeBlock = $(this).text();
-        var currentStorage = localStorage.getItem(currentTimeBlock);
-        if(currentStorage !== null) {
-            $(this).siblings('.description').val(currentStorage);
-        }
-      });
-
-  currentDay.text(dayjs().format('dddd, MMMM D, YYYY h:mm A'));
+  // For ever hour time block, local storage is checked to see if there is saved information so that it persist after refresh and remains in the text field.
+  $(".hour").each(function() {
+      var currentTimeBlock = $(this).text();
+      var currentStorage = localStorage.getItem(currentTimeBlock);
+      if(currentStorage !== null) {
+          $(this).siblings('.description').val(currentStorage);
+      }
+    });
+  
+  // Sets the display for the calendar date and tiem at the top of the page.
+  currentDay.text(dayjs().format('dddd, MMMM D'));
   applyTimeClass();
 });
